af51ef48c443b2e423448b24fae60689d4c45bfa
Jusmicelangelo <141655001+Jusmicelangelo@users.noreply.github.com>
2025-02-06 17:56:28 +0100

    flake

--- app/app/tests.py
+++ app/app/tests.py
@@ -10,16 +10,16 @@ from app import calc
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
 
-
     def test_add_numbers(self):
         """Test addomg numbers together"""
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
-        def test_subtract_numbers(self):
-            """Test subtracting numbers"""
-            res = calc.subtract(15, 10)
 
-            self.assertEqual(res, 5)
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.subtract(15, 10)
+
+        self.assertEqual(res, 5)
 
